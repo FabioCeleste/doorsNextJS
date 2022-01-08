d45b235f5c8217bea9e9d4713629b5c2d2a2f4ac
Fabio <fabio.wow.lol@gmail.com>
2022-01-08 00:46:49 -0300

    final

--- pages/game/[doors]/[hasGift].tsx
+++ pages/game/[doors]/[hasGift].tsx
@@ -8,19 +8,29 @@ import { useRouter } from "next/router";
 
 export default function Game() {
     const router = useRouter()
-     
-    const [p1, setP1] = useState(new DoorModel(1))
-    const [doors, setDoors] = useState(createDoors(50, 2))
+
+    const [doors, setDoors] = useState([])
+    const [valid, setValid] = useState(false)
 
     useEffect(() => {
         const doors = +router.query.doors
         const hasGift = +router.query.hasGift
-        setDoors(createDoors(doors, hasGift))
+ 
+        const validDoors = doors >= 3 && doors <= 100
+        const validGift = hasGift > 0 && hasGift <= doors
 
+        setValid(validDoors && validGift)
+        
+    }, [valid])
+    
+    useEffect(() => {
+        const doors = +router.query.doors
+        const hasGift = +router.query.hasGift
+        setDoors(createDoors(doors, hasGift))
     }, [router.query])
 
     function renderDoors() {
-        return doors.map(door => {
+        return valid && doors.map(door => {
             return <Door key={door.doorNumber} value={door} onChange={
                 newDoor => setDoors(attDoors(doors, newDoor))
             } />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
-import { useState } from "react";
-import Door from "../components/Door";
-import DoorModel from "../model/door";
-import { attDoors, createDoors } from "../functions/doors";
-import Game from "./game/[doors]/[hasGift]";
+import styles from '../styles/Form.module.css'
+import Card from "../components/Card";
+import Link from 'next/link';
+import InputNumber from '../components/InputNumber';
+import { useState } from 'react';
 
-export default function Home() {
+export default function Form() {
+  const [amountDoors, setAmountDoors] = useState(3)
+  const [hasGift, setHasGift] = useState(1)
   
   
   return (
-    <p>Inicio</p>
+    <div className={styles.form}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card>
+          <InputNumber value={amountDoors} onChange={newAmount => setAmountDoors(newAmount)} text='Qtde Portas?' />
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <InputNumber value={hasGift} onChange={newGift => setHasGift(newGift)} text='Com Presente?' />
+        </Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/game/${amountDoors}/${hasGift}`}>
+            <h2 className={styles.start} >Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
+    </div>
   )
 }
